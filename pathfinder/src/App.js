b0b0fed72dfebcbd01f5d85cee23c0437807ad0e
Adonis Puente <adonispuente@MacBook-Pro.attlocal.net>
2020-12-01 17:19:37 -0500

    1 algorithim working

---
+++ pathfinder/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PathfindVisualizer from "./PathFindVisualizer/PathfindVisualizer"
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindVisualizer> </PathfindVisualizer>
+    </div>
+  );
+}
+
+export default App;
